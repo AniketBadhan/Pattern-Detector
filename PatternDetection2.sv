87cfabcea6b5926fc866441b68bb7e65cb2537b9
Aniket Badhan <aniketbadhan@umbc.edu>
2017-10-13 12:26:15 -0400

    Update PatternDetection2.sv

--- PatternDetection2.sv
+++ PatternDetection2.sv
@@ -27,67 +27,67 @@ module PatternDetection2(
 			found_pattern <= 1'b0;
 			case(current_state)
 				RESET		:	begin
-									if(reset_sync) begin
-										if(ack == 1'b1 && data == 8'b01100010) begin
-											current_state <= B1;
-										end
-										else begin
-											current_state <= RESET;
-										end
+								if(reset_sync) begin
+									if(ack == 1'b1 && data == 8'b01100010) begin
+										current_state <= B1;
 									end
 									else begin
 										current_state <= RESET;
 									end
 								end
-				B1			:	begin
-									if(data == 8'b01101111) begin
-										current_state <= O;
-									end
-									else begin
-										current_state <= RESET;
-									end
+								else begin
+									current_state <= RESET;
 								end
-				O			: 	begin
-									if(data == 8'b01101101) begin
-										current_state <= M;
-									end
-									else begin
-										current_state <= RESET;
-									end
+							end
+				B1		:	begin
+								if(data == 8'b01101111) begin
+									current_state <= O;
 								end
-				M			:	begin
-									if(data == 8'b01100010) begin
-										current_state <= B2;
-										found_pattern <= 1'b1;
-									end
-									else begin
-										current_state <= RESET;
-										found_pattern <= 1'b0;
-									end
+								else begin
+									current_state <= RESET;
 								end
-				B2			:	begin
+							end
+				O		: 	begin
+								if(data == 8'b01101101) begin
+									current_state <= M;
+								end
+								else begin
+									current_state <= RESET;
+								end
+							end
+				M		:	begin
+								if(data == 8'b01100010) begin
+									current_state <= B2;
 									found_pattern <= 1'b1;
-									if(ack == 1) begin
-										current_state <= B2;
-									end
-									else begin
-										current_state <= WAIT_ACK;
-									end
 								end
-				WAIT_ACK	:	begin
-									if(ack == 1'b0) begin
-										current_state <= WAIT_ACK;
-										found_pattern <= 1'b1;
-									end
-									else begin
-										current_state <= RESET;
-										found_pattern <= 1'b0;
-									end
-								end
-				default	:	begin
+								else begin
 									current_state <= RESET;
 									found_pattern <= 1'b0;
 								end
+							end
+				B2		:	begin
+								found_pattern <= 1'b1;
+								if(ack == 1) begin
+									current_state <= B2;
+								end
+								else begin
+									current_state <= WAIT_ACK;
+								end
+							end
+				WAIT_ACK	:	begin
+								if(ack == 1'b0) begin
+									current_state <= WAIT_ACK;
+									found_pattern <= 1'b1;
+								end
+								else begin
+									current_state <= RESET;
+									found_pattern <= 1'b0;
+								end
+							end
+				default		:	begin
+								current_state <= RESET;
+								found_pattern <= 1'b0;
+							end
 			
 			endcase
 		end
