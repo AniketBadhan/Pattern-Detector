b30aa3c56f1a9f490612504c483604c6a3e33b76
Aniket Badhan <aniketbadhan@umbc.edu>
2017-10-13 12:28:05 -0400

    Update PatternDetector1.sv

--- PatternDetector1.sv
+++ PatternDetector1.sv
@@ -29,63 +29,63 @@ module PatternDetector1(
 	always @ * begin
 		found_pattern = 0;
 		case(current_state)
-			RESET	:		begin
-								if(ack == 1 && data == 8'b01100010 && reset_sync == 1'b1) begin
-									next_state = B1;
-								end
-								else begin
-									next_state = RESET;
-								end
+			RESET	:	begin
+						if(ack == 1 && data == 8'b01100010 && reset_sync == 1'b1) begin
+							next_state = B1;
+						end
+						else begin
+							next_state = RESET;
+						end
+					end
+			B1	:	begin
+						if(data == 8'b01101111) begin
+							next_state = O;
+						end
+						else begin
+							next_state = RESET;
+						end
 							end
-			B1		:		begin
-								if(data == 8'b01101111) begin
-									next_state = O;
-								end
-								else begin
-									next_state = RESET;
-								end
-							end
-			O		: 		begin
-								if(data == 8'b01101101) begin
-									next_state = M;
-								end
-								else begin
-									next_state = RESET;
-								end
-							end
-			M		:		begin
-								if(data == 8'b01100010) begin
-									next_state = B2;
-									found_pattern = 1;
-								end
-								else begin
-									next_state = RESET;
-									found_pattern = 0;
-								end
-							end
-			B2		:		begin
-								found_pattern = 1;
-								if(ack == 0) begin
-									next_state = WAIT_ACK;
-								end
-								else begin
-									next_state = B2;
-								end
-							end
-			WAIT_ACK:	begin
-								if(ack == 0) begin
-									found_pattern = 1;
-									next_state = WAIT_ACK;
-								end
-								else begin
-									found_pattern = 0;
-									next_state = RESET;
-								end
-							end
-			default:	begin
+			O	:	begin
+						if(data == 8'b01101101) begin
+							next_state = M;
+						end
+						else begin
+							next_state = RESET;
+						end
+					end
+			M	:	begin
+						if(data == 8'b01100010) begin
+							next_state = B2;
+							found_pattern = 1;
+						end
+						else begin
 							next_state = RESET;
 							found_pattern = 0;
 						end
+					end
+			B2	:	begin
+						found_pattern = 1;
+						if(ack == 0) begin
+							next_state = WAIT_ACK;
+						end
+						else begin
+							next_state = B2;
+						end
+					end
+			WAIT_ACK:	begin
+						if(ack == 0) begin
+							found_pattern = 1;
+							next_state = WAIT_ACK;
+						end
+						else begin
+							found_pattern = 0;
+							next_state = RESET;
+						end
+					end
+			default :	begin
+						next_state = RESET;
+						found_pattern = 0;
+					end
 		endcase
 	end
 	
